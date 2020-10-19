0008180a3b80e8062b270db4df9b0762c583f827
DevShrimali <devloper.ds@gmail.com>
2020-10-19 17:37:08 +0530

    compoment build

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Header.css";
+
+function Header() {
+    return (
+        <nav className="header">
+            {/* logo on the left */}
+            <img className="header__logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png" alt=""/>
+            {/*  */}
+            {/* Search */}
+            {/* 3 links */}
+            {/* Basket icon with number */}
+       </nav>
+    )
+}
+
+export default Header
